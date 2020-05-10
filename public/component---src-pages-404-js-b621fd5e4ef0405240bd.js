9e2f5857502aca7cd58671ea5327fe8cd4ef6ab9
Inspiron15I\Kevin <kevin.dicola@gmail.com>
2020-05-10 22:44:48 +0900

    Changed git ignore

---
+++ public/component---src-pages-404-js-b621fd5e4ef0405240bd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{w2l6:function(n,e,t){"use strict";t.r(e);var u=t("q1tI"),w=t.n(u);e.default=function(){return w.a.createElement("p",null,"404")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b621fd5e4ef0405240bd.js.map
