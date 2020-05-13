1c15a56dc46768c74b9ebb304aee974da168821a
DESKTOP-Kevin <kevin.dicola@gmail.com>
2020-05-13 19:44:36 +0900

    Disabled as much bootstrap as i could fine

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ import React from "react"
 
 //import components
 
-import BSHeader from '../components/bsheader'
-import Sidebar from '../components/sidebar'
+//import BSHeader from '../components/bsheader'
+//import Sidebar from '../components/sidebar'
 import BottomNavbar from "../components/bottomnavbar"
 //import Header from '../components/header'
 //import Banner from '../components/banner'
